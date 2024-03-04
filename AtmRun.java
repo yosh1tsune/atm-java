18f95ab54fb09ca887e116661b91fbb58bfddc8d
Bruno Silva <brunorsilva_@hotmail.com>
2024-03-04 19:09:30 -0300

    include org.json lib
    - Add org.json library to handle with JSON inputs and use them to serve the application.
    - Update AtmRun.java to correctly receive and distribute JSON inputs.

--- AtmRun.java
+++ AtmRun.java
@@ -1,19 +1,30 @@
-import models.*;
 import controllers.*;
 
 import java.util.Scanner;
+import org.json.*;
 
 public class AtmRun {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    try {
+      Scanner scanner = new Scanner(System.in);
 
-    String input = scanner.nextLine();
+      String input;
 
-    if (input.equals("caixa")) {
-      RechargesController.recharge(input);
-    }
-    else if (input.equals("saque")) {
-      WithdrawalsController.withdrawal(input);
+      do {
+        input = scanner.nextLine();
+
+        JSONObject json = new JSONObject(input);
+
+        if (json.has("caixa")) {
+          RechargesController.recharge(json.getJSONObject("caixa"));
+        }
+        else if (json.has("saque")) {
+          WithdrawalsController.withdrawal(json.getJSONObject("saque"));
+        }
+      } while (input != "exit");
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      System.out.println(e.getStackTrace());
     }
   }
 }
