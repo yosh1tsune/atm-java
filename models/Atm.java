5cf6f65642e1b15279660ce009198a33a7555948
Bruno Silva <brunorsilva_@hotmail.com>
2024-03-05 14:28:55 -0300

    update class Atm
    - Add constant POSSIBLE_NOTES to inform all the possible values of the
    notes in the ATM, and use that info in the future methods
    - update constructor
    - update class variable atm to a ArrayList of Atm
    - add class method find to select an specific Atm inside the ArrayList atms
    - add instance methods removeNotes and save

--- models/Atm.java
+++ models/Atm.java
@@ -1,26 +1,45 @@
 package models;
 import java.util.ArrayList;
 
+import org.json.JSONObject;
+
 public class Atm {
-  private static ArrayList<String> atms = new ArrayList<>();
+  private static ArrayList<Atm> atms = new ArrayList<>();
 
+  public static final String[] POSSIBLE_NOTES = {
+      "notasDez",
+      "notasVinte",
+      "notasCinquenta",
+      "notasCem"
+    };
 
-  public static ArrayList<String> getAtms() {
+  private String name;
+  private JSONObject notes;
+
+  public static ArrayList<Atm> getAtms() {
     return atms;
   }
 
-  public static void save(String atm) {
-    atms.add(atm);
-  }
+  public static Atm find(String name) {
+    Atm found_atm = null;
 
-  public static String find(String name) {
-    String found_atm = new String();
-
-    for (String atm: atms) {
-      if (atm == name)
-        found_atm = atm;
+    for (Atm atm: atms) {
+      if (atm.name.equals(name))
+        return found_atm = atm;
     }
 
     return found_atm;
+  }
+
+  public void removeNotes(JSONObject usedNotes) {
+    for(String note : POSSIBLE_NOTES) {
+      notes.put(note, notes.getInt(note) - usedNotes.getInt(note));
+    }
+
+    save();
+  }
+
+  public void save() {
+    atms.add(this);
   }
 }
