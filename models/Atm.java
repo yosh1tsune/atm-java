aa43f21436dc8af3a934d2c42d4a26df5b1ba982
Bruno Silva <brunorsilva_@hotmail.com>
2024-03-05 23:56:10 -0300

    update Atm class
    - include new instance variable "status" to store the atm availability
    - include a constructor that don't receive parameters and set the variables
    to pretended initial states
    - include getters and setters

--- models/Atm.java
+++ models/Atm.java
@@ -14,10 +14,21 @@ public class Atm {
     };
 
   private String name;
+  private Boolean status;
   private JSONObject notes;
+
+  public Atm() {
+    this.name = "java";
+    this.status = false;
+    this.notes = new JSONObject("{ notasDez: 0, notasVinte: 0, notasCinquenta: 0, notasCem: 0 }");
+  }
 
   public static ArrayList<Atm> getAtms() {
     return atms;
+  }
+
+  public static Atm create() {
+    return new Atm();
   }
 
   public static Atm find(String name) {
@@ -31,9 +42,35 @@ public class Atm {
     return found_atm;
   }
 
+  public JSONObject toJson() {
+    String json = String.format("{ caixa: { caixaDisponivel: %s, notas: %s, erros: [] } }", status, notes);
+
+    return new JSONObject(json);
+  }
+
+  public void setStatus(Boolean status) {
+    this.status = status;
+  }
+
+  public Boolean getStatus() {
+    return status;
+  }
+
+  public JSONObject getNotes() {
+    return notes;
+  }
+
   public void removeNotes(JSONObject usedNotes) {
     for(String note : POSSIBLE_NOTES) {
       notes.put(note, notes.getInt(note) - usedNotes.getInt(note));
+    }
+
+    save();
+  }
+
+  public void addNotes(JSONObject newNotes) {
+    for(String note : POSSIBLE_NOTES) {
+      notes.put(note, notes.getInt(note) + newNotes.getInt(note));
     }
 
     save();
